2e565abe0475b84f62fa6b23743306c258825827
hsaeyaripour <h.saeyaripour@gmail.com>
2025-07-13 09:17:54 +0000

    Fix test_app.py import for CI consider redirect

--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -6,7 +6,8 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 from app import app
 
-def test_homepage():
+def test_homepage_redirects_to_login():
     client = app.test_client()
     response = client.get('/')
-    assert response.status_code == 200
+    assert response.status_code == 302
+    assert '/login' in response.headers['Location']
